ea9887f333697f6272d6d418fd2e984fa7206940
Ahmed Salah <ahmedsalaheid92@gmail.com>
2025-08-15 01:33:46 +0300

    feat(auth): add JwtAccessStrategy for HS512 access token verification

---
+++ src/presentation/v1/auth/strategies/jwt-access.strategy.ts
@@ -0,0 +1,37 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import type { JwtFromRequestFunction } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import type { RequestUser } from '../types/request-user.type';
+
+type Role = 'ADMIN' | 'MANAGER' | 'EMPLOYEE';
+
+@Injectable()
+export class JwtAccessStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(cfg: ConfigService) {
+    const jwtFromRequest: JwtFromRequestFunction =
+      ExtractJwt.fromAuthHeaderAsBearerToken();
+
+    super({
+      jwtFromRequest,
+      secretOrKey: cfg.getOrThrow<string>('JWT_ACCESS_SECRET', { infer: true }),
+      algorithms: ['HS512'],
+      issuer: 'hr-api',
+      ignoreExpiration: false,
+    });
+  }
+  validate(payload: {
+    sub: string;
+    email: string;
+    role: Role;
+    jti: string;
+  }): RequestUser {
+    return {
+      sub: payload.sub,
+      email: payload.email,
+      role: payload.role,
+      jti: payload.jti,
+    };
+  }
+}
